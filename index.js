d99c14dda1b117721986e978ff8729a52124dca0
Farshad <f.bagdeli13@outlook.com>
2025-07-17 22:39:22 +0200

    add admin.html form and product POST endpoint
    add admin.html to submit new products
    change price from float to string
    add POST /api/products endpoint

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const builder = require('xmlbuilder');
 const products = require('./data');
+const path = require('path');
 
 const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/google-feed.xml', (req, res) => {
   const rss = builder.create('rss', {encoding: 'UTF-8'});
@@ -22,7 +25,7 @@ app.get('/google-feed.xml', (req, res) => {
     item.ele("g:link", {}, product.link);
     item.ele("g:image_link", {}, product.image_link);
     item.ele("g:availability", {}, product.availability);
-    item.ele("g:price", {}, `${product.price.toFixed(2)} ${product.currency}`); 
+    item.ele("g:price", {}, `${product.price} ${product.currency}`); 
     item.ele("g:brand", {}, product.brand);
     item.ele("g:condition", {}, "new");
     item.ele("g:product_type", {}, "Apparel & Accessories > Clothing > Shoes");
@@ -31,7 +34,15 @@ app.get('/google-feed.xml', (req, res) => {
   const xmlString = rss.end({ pretty: true });
   res.header('Content-Type', 'application/rss+xml');
   res.send(xmlString);
+})  
+
+app.post('/app/products', (req, res) => {
+  const { title, description, price, image_link, brand, availability, currency, link } = req.body;
+  
+  products.push({ id: `p${(products.length + 1).toString().padStart(3, '0')}`, title, description, price, image_link, brand, availability, currency, link });
+  res.json({ message: 'Product added successfully' });
 })
+
 
 app.listen(3000, () => { 
   console.log('Google Shopping Feed is available at http://localhost:3000/google-feed.xml');
